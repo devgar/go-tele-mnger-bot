3cab6335eea7ccc7b4ef306d386b43a597e22e2d
Edgar Albalate <dev.gardo@gmail.com>
2018-01-17 10:53:26 +0100

    Initial commit

---
+++ bot.go
@@ -0,0 +1,39 @@
+package main
+
+import("os"; "os/exec"; "log"; "gopkg.in/telegram-bot-api.v4")
+
+var bot *tgbotapi.BotAPI
+
+func processUpdate(update tgbotapi.Update) {
+  text, err := exec.Command("ls", "-s").Output()
+  if err != nil { log.Panic(err) }
+  
+  msg := tgbotapi.NewMessage(update.Message.Chat.ID, string(text))
+  msg.ReplyToMessageID = update.Message.MessageID
+
+  bot.Send(msg)
+}
+
+func initBot(key string) *tgbotapi.BotAPI {
+  apiBot, err :=tgbotapi.NewBotAPI(key)
+  if err != nil { log.Panic(err) }
+  return apiBot
+}
+
+func main() {
+  
+  KEY := os.Getenv("MNGR_TOKEN")
+  bot = initBot(KEY)
+  
+  u := tgbotapi.NewUpdate(0)
+  u.Timeout = 60
+
+  updates, err := bot.GetUpdatesChan(u)
+  if err != nil { log.Panic(err) }
+
+  for update := range updates {
+    if update.Message == nil { continue }
+    log.Printf("Message from %d", update.Message.From.ID)
+    go processUpdate(update)
+  }
+}
